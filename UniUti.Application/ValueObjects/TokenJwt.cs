2abb5ef25b2f58574ec4bc1330fb2360c279bf45
David Santos <gft.davids@grupofleury.com.br>
2022-10-18 12:25:42 -0300

    Firts commit

---
+++ UniUti.Application/ValueObjects/TokenJwt.cs
@@ -0,0 +1,8 @@
+﻿namespace UniUti.Application.ValueObjects
+{
+    public class TokenJwt
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
