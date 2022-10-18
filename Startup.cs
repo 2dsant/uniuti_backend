a5d490a4b122421d63c682b1ed264a9916eaacef
David da Silva Santos <61283730+2dsant@users.noreply.github.com>
2022-10-18 18:56:20 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace projetoteste
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "projetoteste v1"));
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
